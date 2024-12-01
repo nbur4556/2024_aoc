be4942946c16344725ac6cb1744e56599a4e691c
Nick Burt <nbur4556@gmail.com>
2024-12-01 16:51:16 -0600

    setup sample list input

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, Advent of Code 2024!");
+
+int[] list1 = [3, 4, 2, 1, 3, 3];
+int[] list2 = [4, 3, 5, 3, 9, 3];
+
+for(int i = 0; i < list1.Length; i++) {
+    Console.Write(list1[i]);
+    Console.Write("   ");
+    Console.Write(list2[i]);
+    Console.WriteLine("");
+}
