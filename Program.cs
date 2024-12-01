47fe4f86cb330576977ae2166457d41f6cd6ecae
Nick Burt <nbur4556@gmail.com>
2024-12-01 17:07:15 -0600

    locate smallest number in a list

--- Program.cs
+++ Program.cs
@@ -1,11 +1,33 @@
-﻿Console.WriteLine("Hello, Advent of Code 2024!");
+﻿int[] listA = [3, 4, 2, 1, 3, 3];
+int[] listB = [4, 3, 5, 3, 9, 3];
 
-int[] list1 = [3, 4, 2, 1, 3, 3];
-int[] list2 = [4, 3, 5, 3, 9, 3];
+ListComparer listComparer = new ListComparer(
+    [3, 4, 2, 1, 3, 3], 
+    [4, 3, 5, 3, 9, 3]
+);
 
-for(int i = 0; i < list1.Length; i++) {
-    Console.Write(list1[i]);
-    Console.Write("   ");
-    Console.Write(list2[i]);
-    Console.WriteLine("");
+Console.WriteLine(listComparer.LocateSmallestNumber(listComparer._listA));
+Console.WriteLine(listComparer.LocateSmallestNumber(listComparer._listB));
+
+public class ListComparer {
+    //TODO: Should be private if possible
+    public int[] _listA;
+    public int[] _listB;
+
+    public ListComparer(int[] listA, int[] listB) {
+        _listA = listA;
+        _listB = listB;
+    }
+
+    public int LocateSmallestNumber(int[] list) {
+        int smallest = list[0];
+
+        for(int i = 0; i < list.Length; i++) {
+            if (list[i] < smallest) {
+                smallest = list[i];
+            }
+        }
+
+        return smallest;
+    }
 }
