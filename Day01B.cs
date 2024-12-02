304cb35b041944d482ea25e23243ff5a2ac99066
Nick Burt <nbur4556@gmail.com>
2024-12-01 20:16:33 -0600

    create method to calculate similarity scores

--- Day01B.cs
+++ Day01B.cs
@@ -32,7 +32,42 @@ public class Day01B
         }
         finally
         {
-            Console.WriteLine("Solve");
+            SimilarityScorer similarityScorer = new SimilarityScorer
+                (locationWriterA.list, locationWriterB.list);
+
+            int score = similarityScorer.CalculateSimScore();
+            Console.WriteLine(score);
+        }
+    }
+
+    private class SimilarityScorer
+    {
+        private int[] locationsA;
+        private int[] locationsB;
+
+        public SimilarityScorer(int[] listA, int[] listB)
+        {
+            locationsA = listA;
+            locationsB = listB;
+        }
+
+        public int CalculateSimScore()
+        {
+            int score = 0;
+
+            foreach (int locA in locationsA)
+            {
+                int count = 0;
+                foreach (int locB in locationsB)
+                {
+                    if (locA == locB)
+                    {
+                        count++;
+                    }
+                }
+                score = score + (locA * count);
+            }
+            return score;
         }
     }
 }
