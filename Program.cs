65b5a1bdc042c546d5ec727c4f4f962186ad70e6
Nick Burt <nbur4556@gmail.com>
2024-12-01 20:44:03 -0600

    create puzzle runner class

--- Program.cs
+++ Program.cs
@@ -4,8 +4,43 @@
     Console.WriteLine();
 }
 
-// Day01A day01A = new Day01A(args[0]);
-// day01A.Solve();
+PuzzleRunner.Run(args);
 
-Day01B day01B = new Day01B(args[0]);
-day01B.Solve();
+public static class PuzzleRunner
+{
+    public static void Run(String[] args)
+    {
+        Console.Write("Please enter day number: ");
+        String? day = Console.ReadLine()?.Trim();
+        Console.Write("Please enter puzzle number for the day (1 or 2): ");
+        String? puzzle = Console.ReadLine()?.Trim();
+
+        switch (day)
+        {
+            case "1":
+                if (puzzle == "1")
+                {
+                    Day01A day01A = new Day01A(args[0]);
+                    day01A.Solve();
+                }
+                else if (puzzle == "2")
+                {
+                    Day01B day01B = new Day01B(args[0]);
+                    day01B.Solve();
+                }
+                else
+                {
+                    PuzzleNotFound(day, puzzle);
+                }
+                break;
+            default:
+                PuzzleNotFound(day, puzzle);
+                break;
+        }
+    }
+
+    private static void PuzzleNotFound(String? day, String? puzzle)
+    {
+        Console.WriteLine("Puzzle #" + puzzle + " not created on day " + day);
+    }
+}
